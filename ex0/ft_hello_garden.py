09d21b27d4c6458dda41976a42bf7c39b6f25479
bddkalle <9jamh5xf2@mozmail.com>
2026-02-11 11:17:50 +0100

    first push

--- ex0/ft_hello_garden.py
+++ ex0/ft_hello_garden.py
@@ -1,3 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_hello_garden.py                                 :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: fschnorr <fschnorr@student.42berlin.de>    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/02/11 11:17:13 by fschnorr          #+#    #+#              #
+#    Updated: 2026/02/11 11:17:14 by fschnorr         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 """Exercise 0 - Hello Garden.
 
 This module exposes a small function used by main.py.
